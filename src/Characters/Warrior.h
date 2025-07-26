f6334a10c2d9e637cf8fb3300e1f10365c4efcbe
Ryanu01 <ryangomes08090109@gmail.com>
2025-07-26 23:09:33 +0530

    Refactor Warrior class: reorganize includes and member variables for clarity

--- src/Characters/Warrior.h
+++ src/Characters/Warrior.h
@@ -4,13 +4,22 @@
 #include "Physics\RigidBody.h"
 #include "Animation\Animation.h"
 #include "Character.h"
+#include "Physics/Collider.h"
+#include "Physics/Vector2D.h"
+
+#define JUMP_TIME 15.0f
+#define JUMP_FORCE 10.0f
 
 class Warrior : public Character
 {
 private:
     RigidBody* m_RigidBody;
-
     Animation* m_Animation;
+
+    bool m_IsJumping;
+    bool m_IsGrounded;
+    Collider* m_Collider;
+    Vector2D m_LastSafePosition;
     
 public:
     Warrior(Properties *props);
